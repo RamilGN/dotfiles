74e16b0b3b762053b06da34f5d738daf0b1d8426
Ramil Gadzhiev <rami.gaggi@gmail.com>
2022-06-20 22:20:03 +0300

    nvim setup plus nvim config

--- Makefile
+++ Makefile
@@ -2,3 +2,13 @@ ubuntu-prepare:
   apt-get update
   apt-get install git # система контроля версий
   apt-get install xclip # работа с буфером обмена из командной строки
+
+nvim-install:
+	rm -rf nvim/plugin || exit 0
+	rm -rf ~/.local/share/nvim || exit 0
+	rm -rf ~/.config/nvim || exit 0
+	rm -rf $(PACKER_PATH) || exit 0
+	mkdir -p $(PACKER_PATH)
+	git clone --depth 1 https://github.com/wbthomason/packer.nvim $(PACKER_PATH)/packer.nvim
+	ln -snf $(PWD)/nvim ~/.config/nvim
+	nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
---
+++ nvim/init.lua
@@ -0,0 +1,98 @@
+-----------------------------------------------------------
+-- Пакетный менеджер
+-----------------------------------------------------------
+
+-- Установка пакетного менеджера для плагинов
+local packer_install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  packer_bootstrap = vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', packer_install_path})
+end
+
+require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim' -- Пакетный менеджер
+    use 'EdenEast/nightfox.nvim' -- Тема
+end)
+
+-----------------------------------------------------------
+-- Основные настройки 
+-----------------------------------------------------------
+
+-- Работа хоткеев для русской раскладки
+vim.o.langmap = 'ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz'
+-- Номера строк
+vim.o.number = true
+-- Автодополнение
+vim.o.completeopt = 'menu,menuone,noselect'
+-- Игнорируем регистр если в искомой строке нет заглавных букв
+vim.o.ignorecase = true
+vim.o.smartcase = true
+-- Выделение искомой строки
+vim.o.hlsearch = true
+-- Возможность отменять изменения после выключения компьютера/вима
+vim.o.undofile = true
+-- Автоматические отступы
+vim.o.autoindent = true
+-- Цветовая схема
+vim.o.termguicolors = true
+-- Подсвечиваем текущую строку
+vim.o.cursorline = true
+-- Уменьшение времени апдейта
+vim.o.updatetime = 250
+-- Колонка для знаков до номеров строк
+vim.o.signcolumn = 'yes'
+-- Учитывать смещение при поиске
+vim.o.cpoptions = 'n'
+
+-- Выделение для скопированного текста 
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
+-----------------------------------------------------------
+-- Основные хоткеи 
+-----------------------------------------------------------
+
+-- Правильная работа с переносами слов 
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set('n', 'л', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'о', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+-- Пробел в качестве <leader>
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+-- Перемещение между окнами 
+vim.keymap.set('n', '<c-k>', '<c-w><up>')
+vim.keymap.set('n', '<c-j>', '<c-w><down>')
+vim.keymap.set('n', '<c-l>', '<c-w><right>')
+vim.keymap.set('n', '<c-h>', '<C-w><Left>')
+-- Выключаем выделение после поиска
+vim.keymap.set('n', '//', ':nohlsearch<CR>')
+-- Копирование/вставка для системного буфера
+vim.keymap.set({ 'n', 'v' }, '"y', '"+y')
+vim.keymap.set('n', '"Y', '"+Y')
+vim.keymap.set('n', '"p', '"+p')
+vim.keymap.set('n', '"P', '"+P')
+
+-- Редактирование текущей конфигурации
+--map <leader>vl :vsp $MYVIMRC<CR>
+--map <leader>vr :source $MYVIMRC<CR>
+
+-- Редактировние и применение исходного конфига
+vim.keymap.set('n', '<leader>vl', ':vsp $MYVIMRC<CR>')
+vim.keymap.set('n', '<leader>vs', ':source $MYVIMRC<CR>')
+
+-----------------------------------------------------------
+-- Плагины
+-----------------------------------------------------------
+
+-- packer - пакетный менеджер
+-- Использовать команду `:PackerSync` при добавлении, удалениии, изменении конфигурации и тд.
+
+-- nightfox - тема
+vim.cmd('colorscheme nightfox')
