ed6ddc78f62ceee41db65285194b9f442938758f
Ramil Gadzhiev <ramil.hajiyev@insales.ru>
2022-07-11 23:36:04 +0300

    init.lua refactoring

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,63 +1,33 @@
 -----------------------------------------------------------
--- Package manager
+-- # General settings
 -----------------------------------------------------------
 
--- Plugins
-require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim' -- Package manager
-  -- UI
-  use 'EdenEast/nightfox.nvim' -- Theme
-  use { 'nvim-lualine/lualine.nvim',  requires = { 'kyazdani42/nvim-web-devicons', opt = true  } } -- Lower statusline
-  use 'lukas-reineke/indent-blankline.nvim' -- Displaying indents
-  -- Git
-  use 'lewis6991/gitsigns.nvim' -- Git decorations
-  -- Telescope
-  use { 'nvim-telescope/telescope.nvim', requires =  { 'nvim-lua/plenary.nvim' }  } -- Fuzzy finder over lists
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-  -- Treesitter
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use { 'nvim-treesitter/nvim-treesitter-textobjects', requires =  { 'nvim-treesitter/nvim-treesitter' } }
-  use { 'andymass/vim-matchup', requires =  { 'nvim-treesitter/nvim-treesitter' } }
-  -- Other
-  use 'windwp/nvim-autopairs' -- TODO
-  use 'Pocco81/AutoSave.nvim' -- Autosaving
-  use 'folke/which-key.nvim' -- Popup with suggestions to complete a key binding
-  use 'ntpeters/vim-better-whitespace' -- Highlight whitespaces
-  use 'nmac427/guess-indent.nvim' -- Indentation style detection
-  use 'numToStr/Comment.nvim' -- Comment lines with shortcuts
-  use { 'kyazdani42/nvim-tree.lua', requires = { 'kyazdani42/nvim-web-devicons' } } -- File explorer
-end)
-
------------------------------------------------------------
--- General settings
------------------------------------------------------------
-
--- Russian keyboard layout
+-- ## Russian keyboard layout
 vim.opt.langmap = 'ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz'
--- Line numbers
+-- ## Line numbers
 vim.opt.number = true
--- Use interactive zsh
--- vim.opt.shell='zsh -i'
--- Show some lines after cursor
+-- ## Use interactive zsh
+vim.opt.shell='zsh -i'
+-- ## Show some lines after cursor
 vim.opt.scrolloff = 5
--- Location of new vertical split
+-- ## Location of new vertical split
 vim.opt.splitright = true
--- Autocomplete
+-- ## Autocomplete
 vim.opt.completeopt = 'menuone,noselect'
--- Ignore case if there are no capital letters in the search string
+-- ## Ignore case if there are no capital letters in the search string
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
--- Undo after rebooting
+-- ## Undo after rebooting
 vim.opt.undofile = true
--- Term colors
+-- ## Term colors
 vim.opt.termguicolors = true
--- Highlight current line
+-- ## Highlight current line
 vim.opt.cursorline = true
--- Decrease update time
+-- ## Decrease update time
 vim.opt.updatetime = 250
--- Sign clolum
+-- ## Sign clolum
 vim.opt.signcolumn = 'yes'
--- Highlight yanking text
+-- ## Highlight yanking text
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
@@ -68,171 +38,190 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -----------------------------------------------------------
--- General shortcuts
+-- # General shortcuts
 -----------------------------------------------------------
 
--- Russian layout commnad mode
+-- ## Russian layout commnad mode
 vim.keymap.set('n', 'Ж', ':')
--- Word wrapping
+-- ## Word wrapping
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 vim.keymap.set('n', 'л', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'о', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
--- Space as <leader>
+-- ## Space as <leader>
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
--- Move between windows
+-- ## Move between windows
 vim.keymap.set('n', '<C-k>', '<C-w><up>')
 vim.keymap.set('n', '<C-j>', '<C-w><down>')
 vim.keymap.set('n', '<C-l>', '<C-w><right>')
 vim.keymap.set('n', '<C-h>', '<C-w><Left>')
--- Turn off highlight after search
-vim.keymap.set('n', '//', ':nohlsearch<CR>') -- Edit/source current config
+-- ## Turn off highlight after search
+vim.keymap.set('n', '//', ':nohlsearch<CR>')
 vim.keymap.set('n', '<leader>vl', ':vsp $MYVIMRC<CR>')
+-- ## Edit/source current config
 vim.keymap.set('n', '<leader>vs', ':source $MYVIMRC<CR>')
--- Repeat last command
+-- ## Repeat last command
 vim.keymap.set('n', '<leader>re', '@:')
--- Yank/Paste system clipboard
+-- ## Yank/Paste system clipboard
 vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y')
 vim.keymap.set('n', '<leader>Y', '"+Y')
 vim.keymap.set({ 'n', 'v' }, '<leader>p', '"+p')
 vim.keymap.set('n', '<leader>P', '"+P')
 
 -----------------------------------------------------------
--- Plugins
+-- # Plugins
 -----------------------------------------------------------
 
--- packer - package manager
--- Use command `:PackerSync` after changing any plugin configuration
+require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
 
-----------------------------
--- UI
-----------------------------
+  ----------------------------
+  -- ## UI
+  ----------------------------
 
--- nightfox - theme
-vim.cmd('colorscheme nightfox')
+  -- ### Theme
+  use 'EdenEast/nightfox.nvim'
+  vim.cmd('colorscheme nightfox')
 
--- lualine - lower statusline
-require('lualine').setup {
-options = {
-    component_separators = '|',
-    section_separators = '',
-  },
-}
-
--- indent_blankline - displaying indents
-require('indent_blankline').setup {
-  char = '┊',
-}
-
-----------------------------
--- Git
-----------------------------
-
--- gitsigns - git decorations
-require('gitsigns').setup {
-  signs = {
-    add = { text = '+' },
-    change = { text = '~' },
-    delete = { text = '_' },
-    topdelete = { text = '‾' },
-    changedelete = { text = '~' },
-  },
-}
-vim.keymap.set('n', '<leader>gd', '<Cmd>Gitsigns diffthis<CR>')
-vim.keymap.set('n', '<leader>gh', '<Cmd>Gitsigns preview_hunk<CR>')
-vim.keymap.set('n', '<leader>gr', '<Cmd>Gitsigns reset_hunk<CR>')
-vim.keymap.set('n', '<leader>gn', '<Cmd>Gitsigns next_hunk<CR>')
-vim.keymap.set('n', '<leader>gN', '<Cmd>Gitsigns prev_hunk<CR>')
-
-----------------------------
--- Telescope
-----------------------------
-
--- telescope -- fuzzy finder over lists
-require('telescope').setup()
-require('telescope').load_extension('fzf')
-vim.keymap.set('n', '<leader>b', '<Cmd>Telescope buffers<CR>')
-vim.keymap.set('n', '<leader>tf', '<Cmd>Telescope find_files<CR>')
-vim.keymap.set('n', '<leader>tb', '<Cmd>Telescope current_buffer_fuzzy_find<CR>')
-vim.keymap.set('n', '<leader>tl', '<Cmd>Telescope live_grep<CR>')
-vim.keymap.set('n', '<leader>ts', '<Cmd>Telescope grep_string<CR>')
-vim.keymap.set('n', '<leader>td', '<Cmd>Telescope diagnostics<CR>')
-vim.keymap.set('n', '<leader>tg', '<Cmd>Telescope git_commits<CR>')
-vim.keymap.set('n', '<leader>tr', '<Cmd>Telescope lsp_references<CR>')
-vim.keymap.set('n', '<leader>to', '<Cmd>Telescope lsp_document_symbols<CR>')
-vim.keymap.set('n', '<leader>ta', '<Cmd>Telescope lsp_range_code_actions<CR>')
-vim.keymap.set('n', '<leader>th', '<Cmd>Telescope help_tags<CR>')
-
-----------------------------
--- Treesitter
-----------------------------
-require('nvim-treesitter.configs').setup {
-  highlight = { enable = true },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = 'gnn',
-      node_incremental = 'grn',
-      scope_incremental = 'grc',
-      node_decremental = 'grm',
+  -- ### Fancy lower statusline
+  use { 'nvim-lualine/lualine.nvim',  requires = { 'kyazdani42/nvim-web-devicons', opt = true  } }
+  require('lualine').setup{
+    options = {
+      component_separators = '|',
+      section_separators = '',
     }
-  },
-  indent = {
-    enable = true,
-    disable = { 'ruby' }
-  },
-  -- TODO
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true,
-      keymaps = {
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
-      }
-    }
-  },
-  matchup = {
-     enable = true
   }
-}
-vim.g.matchup_matchparen_deferred = 1
--- TODO: WHEREAMI
 
-----------------------------
--- Other
-----------------------------
+  -- ### Displaying indents
+  use 'lukas-reineke/indent-blankline.nvim'
+  require('indent_blankline').setup {
+    char = '┊',
+  }
 
--- nvim-autopairs
-require('nvim-autopairs').setup()
+  ----------------------------
+  -- ## Git
+  ----------------------------
 
--- autosave
-require('autosave').setup {
-  execution_message = ''
-}
+  -- ### Decorations
+  use 'lewis6991/gitsigns.nvim'
+  require('gitsigns').setup {
+    signs = {
+      add = { text = '+' },
+      change = { text = '~' },
+      delete = { text = '_' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '~' },
+    },
+  }
+  vim.keymap.set('n', '<leader>gd', '<Cmd>Gitsigns diffthis<CR>')
+  vim.keymap.set('n', '<leader>gh', '<Cmd>Gitsigns preview_hunk<CR>')
+  vim.keymap.set('n', '<leader>gr', '<Cmd>Gitsigns reset_hunk<CR>')
+  vim.keymap.set('n', '<leader>gn', '<Cmd>Gitsigns next_hunk<CR>')
+  vim.keymap.set('n', '<leader>gN', '<Cmd>Gitsigns prev_hunk<CR>')
 
--- whichkey - popup with suggestions to complete a key binding
-require('which-key').setup()
+  ----------------------------
+  -- ## Telescope
+  ----------------------------
 
--- vim-better-whitespace - hightlight trailing whitespaces
-vim.g.better_whitespace_enabled = true
-vim.g.better_whitespace_ctermcolor = 'DarkRed'
-vim.g.better_whitespace_guicolor = 'DarkRed'
+  -- Fuzzy finder over lists with fzf extension
+  use { 'nvim-telescope/telescope.nvim', requires =  { 'nvim-lua/plenary.nvim' }  }
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+  require('telescope').setup()
+  require('telescope').load_extension('fzf')
+  vim.keymap.set('n', '<leader>b', '<Cmd>Telescope buffers<CR>')
+  vim.keymap.set('n', '<leader>tf', '<Cmd>Telescope find_files<CR>')
+  vim.keymap.set('n', '<leader>tb', '<Cmd>Telescope current_buffer_fuzzy_find<CR>')
+  vim.keymap.set('n', '<leader>tl', '<Cmd>Telescope live_grep<CR>')
+  vim.keymap.set('n', '<leader>ts', '<Cmd>Telescope grep_string<CR>')
+  vim.keymap.set('n', '<leader>td', '<Cmd>Telescope diagnostics<CR>')
+  vim.keymap.set('n', '<leader>tg', '<Cmd>Telescope git_commits<CR>')
+  vim.keymap.set('n', '<leader>tr', '<Cmd>Telescope lsp_references<CR>')
+  vim.keymap.set('n', '<leader>to', '<Cmd>Telescope lsp_document_symbols<CR>')
+  vim.keymap.set('n', '<leader>ta', '<Cmd>Telescope lsp_range_code_actions<CR>')
+  vim.keymap.set('n', '<leader>th', '<Cmd>Telescope help_tags<CR>')
+  -- require'telescope.builtin'.grep_string{ shorten_path = true, word_match = "-w", only_sort_text = true, search = '' } -- TODO
 
--- guess-indent - indentation style detection
-require('guess-indent').setup()
+  ----------------------------
+  -- ## Treesitter --TODO
+  ----------------------------
 
--- Comment - comment lines with shortcuts
-require('Comment').setup()
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use { 'nvim-treesitter/nvim-treesitter-textobjects', requires =  { 'nvim-treesitter/nvim-treesitter' } }
+  use { 'andymass/vim-matchup', requires =  { 'nvim-treesitter/nvim-treesitter' } }
+  require('nvim-treesitter.configs').setup {
+    highlight = { enable = true },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = 'gnn',
+        node_incremental = 'grn',
+        scope_incremental = 'grc',
+        node_decremental = 'grm',
+      }
+    },
+    indent = {
+      enable = true,
+      disable = { 'ruby' }
+    },
+    -- TODO
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          ['af'] = '@function.outer',
+          ['if'] = '@function.inner',
+          ['ac'] = '@class.outer',
+          ['ic'] = '@class.inner',
+        }
+      }
+    },
+    matchup = {
+      enable = true
+    }
+  }
+  -- vim.g.matchup_matchparen_deferred = 1
+  -- TODO: WHEREAMI
 
--- nvim-tree - file explorer
-require('nvim-tree').setup()
+  ----------------------------
+  -- ## Other
+  ----------------------------
 
-vim.keymap.set('n', '<leader><leader>', ':NvimTreeFindFileToggle<CR>')
+  -- ### Autopairs
+  use 'windwp/nvim-autopairs' -- TODO
+  require('nvim-autopairs').setup()
+
+  -- ### Autosaving
+  use 'Pocco81/AutoSave.nvim'
+  require('autosave').setup {
+    execution_message = ''
+  }
+
+  -- ### Popup with suggestions to complete a key binding
+  use 'folke/which-key.nvim' -- Popup with suggestions to complete a key binding
+  require('which-key').setup()
+
+  -- ### Hightlight trailing whitespaces
+  use 'ntpeters/vim-better-whitespace'
+  vim.g.better_whitespace_enabled = true
+  vim.g.better_whitespace_ctermcolor = 'DarkRed'
+  vim.g.better_whitespace_guicolor = 'DarkRed'
+
+  -- ### Indentation style detection
+  use 'nmac427/guess-indent.nvim'
+  require('guess-indent').setup()
+
+  -- ### Comment lines with shortcuts
+  use 'numToStr/Comment.nvim' -- Comment lines with shortcuts
+  require('Comment').setup()
+
+  -- ### File explorer
+  require('nvim-tree').setup()
+  use { 'kyazdani42/nvim-tree.lua', requires = { 'kyazdani42/nvim-web-devicons' } } -- File explorer
+  vim.keymap.set('n', '<leader><leader>', ':NvimTreeFindFileToggle<CR>')
+end)
 
 -----------------------------------------------------------
 -- TODO
