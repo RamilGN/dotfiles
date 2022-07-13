721c6b95a8bea13672a13f54e5b467cb5443e604
Ramil Gadzhiev <rami.gaggi@gmail.com>
2022-07-13 23:10:59 +0300

    Add LSP

--- nvim/init.lua
+++ nvim/init.lua
@@ -189,6 +189,52 @@ require('packer').startup(function(use)
   -- TODO: WHEREAMI
 
   ----------------------------
+  -- ## LSP
+  ----------------------------
+
+  use 'williamboman/nvim-lsp-installer'
+  use 'neovim/nvim-lspconfig'
+  require('nvim-lsp-installer').setup {
+    automatic_installation = true
+  }
+
+  -- Diagnositc mappings
+  local opts = { noremap=true, silent=true }
+  vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
+  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+  vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+  vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
+
+  -- General LSP setting
+  local on_attach = function(client, bufnr)
+    -- Mappings.
+    local bufopts =  { buffer=bufnr }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+    vim.keymap.set('n', '<leader>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, bufopts)
+    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', '<leader>ff', vim.lsp.buf.formatting, bufopts)
+  end
+
+  local lspconfig = require('lspconfig')
+  local servers = { 'sumneko_lua' }
+  for _, lsp in ipairs(servers) do
+    lspconfig[lsp].setup {
+      on_attach = on_attach
+    }
+  end
+
+  ----------------------------
   -- ## Other
   ----------------------------
 
